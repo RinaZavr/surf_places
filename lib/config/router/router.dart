723329d32f2e531940e65ae2676af069306e5fbb
Rina Bard <katrinbardal@gmail.com>
2025-08-01 11:36:02 +0300

    feat: Added talker and register dio in getit

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:surf_places/config/router/routes.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 abstract class AppRouter {
   const AppRouter._();
 
   static final router = GoRouter(
     debugLogDiagnostics: kDebugMode,
+    observers: [TalkerRouteObserver(GetIt.I.get<Talker>())],
     routes: $appRoutes,
     initialLocation: TestRoute().location,
   );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:places_api/api_client.dart';
 import 'package:surf_places/feature/app.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
+import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
+import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton<Talker>(talker);
+
+  GetIt.I.registerSingleton<DioClient>(
+    DioClient(
+      baseUrl: '',
+      logger: TalkerDioLogger(
+        talker: talker,
+        settings: TalkerDioLoggerSettings(
+          printRequestHeaders: true,
+          printResponseData: false,
+        ),
+      ),
+    ),
+  );
+
+  Bloc.observer = TalkerBlocObserver(talker: talker);
+
   runApp(const App());
 }
