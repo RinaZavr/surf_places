7ac5e4a5a4fec08b85adb761d871aa4caf2fa9ab
Rina Bard <katrinbardal@gmail.com>
2025-08-01 17:36:32 +0300

    feat: Added icons for wrapper

---
+++ lib/common/consts/icons_consts.dart
@@ -0,0 +1,13 @@
+abstract class AppIcons {
+  static const String wrapperList = 'assets/icons/wrapper/List.svg';
+  static const String wrapperListSelected =
+      'assets/icons/wrapper/List_Fill.svg';
+  static const String wrapperMap = 'assets/icons/wrapper/Map.svg';
+  static const String wrapperMapSelected = 'assets/icons/wrapper/Map_Fill.svg';
+  static const String wrapperFavorites = 'assets/icons/wrapper/Heart.svg';
+  static const String wrapperFavoritesSelected =
+      'assets/icons/wrapper/Heart_Fill.svg';
+  static const String wrapperSettings = 'assets/icons/wrapper/Settings.svg';
+  static const String wrapperSettingsSelected =
+      'assets/icons/wrapper/Settings_Fill.svg';
+}
