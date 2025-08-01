e03a67220ddeb7c798979a2f01234cdc69f51368
Rina Bard <katrinbardal@gmail.com>
2025-08-01 16:20:46 +0300

    feat: Added color to shadows

--- lib/config/styles/shadows.dart
+++ lib/config/styles/shadows.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:surf_places/config/styles/colors.dart';
 
 abstract class AppShadows {
   const AppShadows._();
 
-  static const smallShadow = BoxShadow(blurRadius: 8, offset: Offset(0, 2));
-  static const cardsShadow = BoxShadow(blurRadius: 16, offset: Offset(0, 4));
+  static const smallShadow = BoxShadow(
+    color: AppColors.shadowSmall,
+    blurRadius: 8,
+    offset: Offset(0, 2),
+  );
+  static const cardsShadow = BoxShadow(
+    color: AppColors.shadowCards,
+    blurRadius: 16,
+    offset: Offset(0, 4),
+  );
 }
